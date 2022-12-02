e27e80ded2e6c41e8db73bfee8c11815c7c761c2
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2022-12-03 02:17:09 +0300

    update the task 4

--- src/Task4.java
+++ src/Task4.java
@@ -1,19 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task4 {
     public static void main(String[] args) {
-        int res = 0;
-        int[][] array = new int[][]{
-                {1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 10, 11, 12}
-        };
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+        int sum = 0;
+
+        System.out.print("Enter i = ");
+        while (!input.hasNextInt()) {
+            System.out.println("you need to enter a number!!! try again :)");
+            input.nextLine();
+        }
+        int line = input.nextInt();
+
+        System.out.print("Enter j = ");
+        while (!input.hasNextInt()) {
+            System.out.println("you need to enter a number!!! try again :)");
+            input.nextLine();
+        }
+        int column = input.nextInt();
+
+        int[][] array = new int[line][column];
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                res += array[i][j];
+                array[i][j] = random.nextInt(10);
+                System.out.print(array[i][j] + " ");
+
+                sum += array[i][j];
             }
+            System.out.println();
         }
-        System.out.println(res);
+        System.out.println("sum = " + sum);
     }
-
-
 }
