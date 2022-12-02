698f51de86f43c6822edf07d3f65257c9fbde8d0
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2022-12-03 01:28:15 +0300

    update the task 7

--- src/Task7.java
+++ src/Task7.java
@@ -1,9 +1,13 @@
 public class Task7 {
     public static void main(String[] args) {
         int[][] hourglass = new int[7][7];
+
+
         for (int i = 0; i < hourglass.length; i++) {
             for (int j = 0; j < hourglass.length; j++) {
-                if (j >= i || i + j > hourglass.length - 2) {
+                if (j >= i && i + j < hourglass.length ) {
+                    hourglass[i][j] = 1;
+                } else if (j <= i && i + j + 2 > hourglass.length) {
                     hourglass[i][j] = 1;
                 } else {
                     hourglass[i][j] = 0;
